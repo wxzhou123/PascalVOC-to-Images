787def958c418eb34829663a3578536f189587cf
wxzhou123 <wxzhou@163.com>
2022-06-21 01:38:47 +0800

    增加voc转换yolo格式

--- main.py
+++ main.py
@@ -6,23 +6,26 @@ import pprint
 
 from PIL import Image
 
-
 # Printing results
 pp = pprint.PrettyPrinter(indent=4)
+
+pascal_voc_contents = []
 
 
 # Look for XML files and parses then as if they were Pascal VOC Files
 def process():
     # Finds all XML files on data/ and append to list
-    pascal_voc_contents = []
-    os.chdir("data")
+    os.chdir("f:/radar/labeldata")
 
     print("Found {} files in data directory!".format(
         str(len(glob.glob("*.xml")))))
     for file in glob.glob("*.xml"):
-        f_handle = open(file, 'r')
+        filename = 'f:/radar/labeldata/' + str(file)
+        f_handle = open(filename, 'r', encoding='utf-8')
         print("Parsing file '{}'...".format(file))
-        pascal_voc_contents.append(xmltodict.parse(f_handle.read()))
+        xml = f_handle.read()
+        pascal_voc_contents.append(xmltodict.parse(xml))
+        f_handle.close()
 
     # Process each file individually
     for index in pascal_voc_contents:
@@ -44,7 +47,7 @@ def extractDataset(dataset):
     img = Image.open(dataset['filename'])
 
     # Create output directory
-    save_dir = dataset['filename'].split('.')[0]
+    save_dir = 'images'  # dataset['filename'].split('.')[0]
     try:
         os.mkdir(save_dir)
     except:
@@ -57,6 +60,9 @@ def extractDataset(dataset):
     # Run through each item and save cut image to output folder
     for item in dataset['object']:
         # Convert str to integers
+        isend = item == 'name'
+        if isend:
+            item = dataset['object']
         bndbox = dict([(a, int(b)) for (a, b) in item['bndbox'].items()])
         # Crop image
         im = img.crop((bndbox['xmin'], bndbox['ymin'],
@@ -64,6 +70,9 @@ def extractDataset(dataset):
         # Save
         im.save(sample_preamble + str(i) + '.jpg')
         i = i + 1
+        if isend:
+            break
+
 
 if __name__ == '__main__':
     print("\n------------------------------------")
